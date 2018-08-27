687c81a6919e4d9a214df931f1b8e74bcc7931c4
Curt Caldwell, Derek Hammer, Eddie Harris, Eliot Charette, James Hanley and Swati Sahay <curt+derek+eddie+eliot+james+swati@>
2018-08-27 09:22:21 -0700

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FeaturedComponent } from './featured/featured.component';
+import { SearchComponent } from './search/search.component';
+import { DealsComponent } from './deals/deals.component';
+import { LoginComponent } from './login/login.component';
+import { ResultsComponent } from './results/results.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FeaturedComponent,
+    SearchComponent,
+    DealsComponent,
+    LoginComponent,
+    ResultsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
