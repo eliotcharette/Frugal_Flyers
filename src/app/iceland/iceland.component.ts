b0e9f31491d66b0b0ea823f32bfbbefa848f02d5
curt caldwell <curt.caldwell24@gmail.com>
2018-08-27 14:24:38 -0700

    add iceland component

---
+++ src/app/iceland/iceland.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iceland',
+  templateUrl: './iceland.component.html',
+  styleUrls: ['./iceland.component.css']
+})
+export class IcelandComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
