6d248c8210c6533869c8dd509438282ed9a6a3c5
curt caldwell <curt.caldwell24@gmail.com>
2018-08-27 11:22:40 -0700

    add result component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { routing } from './app.routing';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -26,7 +28,10 @@ import { ProfileComponent } from './profile/profile.component';
     ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search.model.ts
+++ src/app/search.model.ts
@@ -1,3 +1,3 @@
 export class Search {
-  constructor(public departure: string, public arrival: string, public depatureDate: string, public returnDate: string, public passengers: number )
+  constructor(public departure: string, public arrival: string, public depatureDate: string, public returnDate: string, public passengers: number ) { }
 }
