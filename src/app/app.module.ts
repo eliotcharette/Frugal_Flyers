436045bbd62e31c10b35e788944eaef57f49a3ae
James Hanley <hanley.doth@outlook.com>
2018-08-27 12:03:54 -0700

    Add necessary modules and things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -26,6 +26,9 @@ import { ProfileComponent } from './profile/profile.component';
     ProfileComponent
   ],
   imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule,
     BrowserModule
   ],
   providers: [],
