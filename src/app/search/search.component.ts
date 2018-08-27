06b1e80c2046723ac92a3746b583cbc902332cd7
Eddie Harris and Swati Sahay <swati@>
2018-08-27 13:34:29 -0700

    add api

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { apiKey } from './../env';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -8,7 +9,7 @@ import 'rxjs/add/operator/map';
 })
 export class SearchComponent  {
 
-  private apiUrl = 'https://api.sandbox.amadeus.com/v1.2/flights/affiliate-search?apikey=vEOvRjNVE3RgxiYW3JLG4VJcEO9W6RwP&origin=LON&destination=LAX&departure_date=2018-12-30&adults=1'
+  private apiUrl = 'https://api.sandbox.amadeus.com/v1.2/flights/affiliate-search?origin=LON&destination=LAX&departure_date=2018-12-30&adults=1&apikey='+apiKey()
    data: any = {};
    constructor(private http: Http){
      console.log("hello");
