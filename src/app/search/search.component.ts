ab3cf07a7c0a9fe0897b2b0076da67df8f3e188e
James Hanley <hanley.doth@outlook.com>
2018-08-28 14:42:41 -0700

    fixed some errors

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -10,40 +10,39 @@ import { apiKey } from './../env';
 })
 export class SearchComponent  {
 
+  public adults: Adult[] = [
+      {value: '1', viewValue: '1'},
+      {value: '2', viewValue: '2'},
+      {value: '3', viewValue: '3'},
+      {value: '4', viewValue: '4'},
+      {value: '5', viewValue: '5'},
+      {value: '6', viewValue: '6'},
+      {value: '7', viewValue: '7'},
+      {value: '8', viewValue: '8'},
+      {value: '9', viewValue: '9'}
+    ];
 
   private apiUrl = 'https://api.sandbox.amadeus.com/v1.2/flights/affiliate-search?origin=LON&destination=LAX&departure_date=2018-12-30&adults=1&apikey='+apiKey()
 
 
    data: any = {};
-   constructor(private http: Http){
-     console.log("hello");
+   constructor(private http: Http) {
+     console.log('hello');
      this.getResult();
      this.getData();
    }
-   getData()
-   {
+   getData() {
      return this.http.get(this.apiUrl)
-     .map((res: Response) => res.json())
+     .map((res: Response) => res.json());
    }
-   getResult()
-   {
+   getResult() {
      this.getData().subscribe(data => {
        console.log(data);
 
-       this.data =data;
-      })
+       this.data = data;
+     });
    }
-   adults: Adult[] = [
-       {value: '1', viewValue: '1'},
-       {value: '2', viewValue: '2'},
-       {value: '3', viewValue: '3'},
-       {value: '4', viewValue: '4'},
-       {value: '5', viewValue: '5'},
-       {value: '6', viewValue: '6'},
-       {value: '7', viewValue: '7'},
-       {value: '8', viewValue: '8'},
-       {value: '9', viewValue: '9'}
-     ];
+
 }
 
 
