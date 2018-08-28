fe532fc3bfc5a3cf0bbbc37e10e5c40a583ce8cc
James Hanley <hanley.doth@outlook.com>
2018-08-28 08:25:27 -0700

    update necessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatRippleModule } from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatRippleModule, MatToolbarModule, MatAutocompleteModule, MatDatepickerModule, MatRadioModule, MatSelectModule, MatSliderModule, MatSlideToggleModule, MatButtonToggleModule, MatTooltipModule, MatIconModule, MatMenuModule, MatSidenavModule, MatListModule, MatGridListModule, MatCardModule, MatStepperModule, MatTabsModule, MatExpansionModule, MatChipsModule, MatProgressSpinnerModule, MatProgressBarModule, MatDialogModule, MatSnackBarModule, MatTableModule, MatSortModule, MatPaginatorModule } from '@angular/material';
 import { routing } from './app.routing';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { NavheaderComponent } from './navheader/navheader.component';
 import { FooterComponent } from './footer/footer.component';
 import { FeaturedComponent } from './featured/featured.component';
 import { SearchComponent } from './search/search.component';
@@ -21,7 +21,7 @@ import { IcelandComponent } from './iceland/iceland.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    NavheaderComponent,
     FooterComponent,
     FeaturedComponent,
     SearchComponent,
@@ -34,11 +34,37 @@ import { IcelandComponent } from './iceland/iceland.component';
     IcelandComponent
   ],
   imports: [
-    MatButtonModule,
     MatCheckboxModule,
-    MatFormFieldModule,
+    MatCheckboxModule,
+    MatButtonModule,
     MatInputModule,
-    MatRippleModule,
+    MatAutocompleteModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatDialogModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
