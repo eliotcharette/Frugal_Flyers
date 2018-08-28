34b38052507a9c7b35fb4c64f68eeeb7edb21484
James Hanley <hanley.doth@outlook.com>
2018-08-28 09:54:14 -0700

    Add header, fixed whitespace in routing, changed order for app component.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,4 +1,4 @@
-import { ModuleWithProviders }  from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { ResultsComponent } from './results/results.component';
