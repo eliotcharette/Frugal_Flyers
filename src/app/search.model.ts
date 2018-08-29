3d83f888daa255378e3a4799082c0bc2493fbb6d
Swati Sahay <swatiranjan0111@gmail.com>
2018-08-29 10:57:59 -0700

    add max price

--- src/app/search.model.ts
+++ src/app/search.model.ts
@@ -1,4 +1,4 @@
 
 export class Search {
-  constructor(public departure: string, public arrival: string, public depatureDate: string, public returnDate: string, public passengers: number ) { }
+  constructor(public departure: string, public arrival: string, public depatureDate: string, public returnDate: string, public passengers: number, public maxPrice: number ) { }
 }
